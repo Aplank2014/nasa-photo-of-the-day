29bc5c4a6e2cb477b0e1077a156b65586e1d6fbd
Angela Plank <angelaplanknm@gmail.com>
2020-06-25 10:01:57 -0600

    first commit

--- src/Photos.js
+++ src/Photos.js
@@ -2,31 +2,24 @@ import React, { useState, useEffect  } from "react";
 import axios from "axios";
 import Header from "./Header";
 
-
-
 function Photos() {
-  const [nasaData, setNasaData] = useState({})
+  const [nasaData, setNasaData] = useState([]);
  
   useEffect(() => {
       axios.get('https://api.nasa.gov/planetary/apod?api_key=e8x7uyaDvmfbCPAvREOePG4TLeQLPmymlVQdDDK5')
       .then(res => {
-          console.log(res.data) 
-          setNasaData(res.data)
-      })
-          
-
+         // console.log(res.data) 
+          setNasaData(res.data);
+      });
   }, []);
 
   return (
       <div className="Container">
-      
               <div className="entry">
               <Header {...nasaData} />
               <Photos {...nasaData} />
-
               </div>
-      
-          </div>
+      </div>
   )
 }
 export default Photos
