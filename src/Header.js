0e81fabf64fe85efd7bb45dae4b427fc8cdb5812
Angela Plank <angelaplanknm@gmail.com>
2020-06-28 15:52:05 -0600

    Met MVP

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,39 @@
-import React from 'react'
- 
+import React from 'react';
+import styled from 'styled-components';
+
+
+
+const NavBarDiv = styled.nav`
+display: flex;
+justify-content: center;
+align-content: center;
+width: 100%;
+margin-top: 15px;
+background-color: lightblue;
+background-repeat: repeat;
+background-size: 200px 200px;
+`;
+
+const Title = styled.h1`
+  font-size: 100px;
+  text-align: center;
+  color: royalblue;
+`;
 
 const Header = props => {
   console.log(props, "props")
   return (
-      <div className="Header" key="">
-            <h1>NASA Photo of the Day!</h1>
-            <h3>{props.title}</h3> 
-            <h4>{props.date}</h4> 
-        </div>
+    <NavBarDiv>
+      {props.data && 
+      <div>
+        <Title>NASA Photo of the Day!</Title>
+        <h3>{props.data.title}</h3>
+        <h3>{props.data.date}</h3>
+      </div>
+      }
+    </NavBarDiv>
     )
   }
-
 
 export default Header;
 
